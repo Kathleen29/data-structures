60404224f978bdfe7eb4742784a7d5cf03d3c364
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-10-23 18:12:32 -0400

    Implement enqueue method to store value at numeric key

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,32 @@
 var Queue = function() {
+// someInstance stores .enqueue, .dequeue, .size
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  // numeric keys represent in what order the function was called??
   var storage = {};
+  var keyNum = 1;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // set storage at [] numeric value to be the input value
+    storage[keyNum] = value;
+    keyNum++;
   };
 
   someInstance.dequeue = function() {
+    // Find the lowest numeric key value within storage, return the value
   };
 
   someInstance.size = function() {
+    // Count the number of properties within the storage object
+    // return the value
   };
 
   return someInstance;
 };
+
+// [[1, a], [2, b], [3, c]]
+// remove 1
+// [[1, b], [2, c]] <--
