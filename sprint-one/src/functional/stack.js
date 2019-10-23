1095d9caf2db6602bf713d116203abda64aefa05
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-10-23 18:51:01 -0400

    Implemented size and push methods

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var keyNum = 1;
   // Implement the methods below
   someInstance.push = function(value) {
+    // track numerical key and insert the value at numerican key in the object
+    storage[keyNum] = value;
+    // increment numerical key
+    keyNum++;
+    // return storage object
+    return storage;
   };
-
+  // return most recently added value and remove that value from storage obj
   someInstance.pop = function() {
+    // use Objects.keys()
+    // set the last key in object to a variable
+    // delete the last key from the storage object
+    // return the last key variable
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
