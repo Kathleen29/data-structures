71364f2bb80a8831f605316b2c320ebb8ef2fe8b
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-10-23 18:33:18 -0400

    Implement dequeue and size methods to return the least recently added value and return storage size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -13,15 +13,29 @@ var Queue = function() {
     // set storage at [] numeric value to be the input value
     storage[keyNum] = value;
     keyNum++;
+    return storage;
   };
 
   someInstance.dequeue = function() {
     // Find the lowest numeric key value within storage, return the value
+    var keysArr = Object.keys(storage);
+    var lowest = parseInt(keysArr[0]);
+    var string;
+    for (var i = 1; i < keysArr.length; i++) {
+      if (parseInt(keysArr[i]) < lowest) {
+        lowest = parseInt(keysArr[i]);
+      }
+    }
+    string = storage[lowest];
+    delete storage[lowest];
+    return string;
+    // delete storage.something
   };
 
   someInstance.size = function() {
     // Count the number of properties within the storage object
     // return the value
+    return Object.keys(storage).length;
   };
 
   return someInstance;
