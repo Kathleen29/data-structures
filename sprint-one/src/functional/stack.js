15f432bf1cc629b9af469da7dfc2f212587f27ab
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-10-23 19:07:10 -0400

    Implement pop method to return most recent value added to object

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -16,9 +16,15 @@ var Stack = function() {
   // return most recently added value and remove that value from storage obj
   someInstance.pop = function() {
     // use Objects.keys()
+    var keysArr = Object.keys(storage);
     // set the last key in object to a variable
-    // delete the last key from the storage object
-    // return the last key variable
+    if(keysArr.length >= 1){
+      var last = storage[keysArr[keysArr.length-1]];
+      // delete the last key from the storage object
+      delete storage[keysArr[keysArr.length-1]];
+      // return the last key variable
+      return last;
+    }
   };
 
   someInstance.size = function() {
