051fde9cdc85f986c8df3ed96a5380146b72b9d5
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-10-24 18:44:43 -0400

    Implement the prototypal methods for stack, push, pop, size

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create a storage object where stack methods will be added
+  var storage = Object.create(stackMethods);
+  // create object where the stack will live
+  storage.stack = {};
+  // return storage object
+  return storage;
 };
 
 var stackMethods = {};
+var keyNum = 1;
 
+stackMethods.push = function(value){
+  // define the value at key number
+  this.stack[keyNum] = value;
+  // increment the key
+  keyNum++;
+  // return the stack
+  return this.stack;
+};
 
+stackMethods.pop = function(){
+  // store object keys in an array
+  var keys = Object.keys(this.stack);
+  // store the largest value in a variable
+  var largest = this.stack[keys[keys.length-1]];
+  // delete the largest key value pair from the storage stack
+  delete this.stack[keys[keys.length-1]];
+  // return the largest value variable
+  return largest;
+};
+
+stackMethods.size = function(){
+  // return the length of the object.key(storage) array
+  return Object.keys(this.stack).length;
+};
