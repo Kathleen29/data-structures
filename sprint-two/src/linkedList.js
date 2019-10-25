74b6926583a3fda11c501eb515367bb8757fa642
Camryn Reid <camrr1991@gmail.com>
2019-10-25 12:07:27 -0700

    Implemented addToTail, removeHead, and contains methods of LinkedList class

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,22 +5,50 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     // call an instance of Node
+    var instance = Node(value);
     // somehow update previous node to tell it what next should point to
+      // check if list object is empty... if yes
+    if (this.head === null) {
+      // set head and tail to that node
+      this.head = instance;
+      this.tail = instance;
+    } // else
+    else {
+      // set next of current tail node to equal this instance of node and set this instance to tail
+      this.tail.next = instance;
+      this.tail = instance;
     // add new instance of node to list object
+    }
   };
 
   list.removeHead = function() {
-    // head gets reassigned to next of current head node
     // store original node that head points to in variable
+    var currentHead = this.head.value;
+    // head gets reassigned to next of current head node
+    this.head = this.head.next;
     // return original node variable
+    return currentHead;
   };
 
   list.contains = function(target) {
-    // loop through linked list for target value
-    // return true if target value is found
-    // return false if target value is not found
+    // create a recursive function to find target in chain of nodes
+    var start = this.head;
+    var found;
+    var traverse = function (node) {
+      // return true if target value is found
+      if (node.value === target) {
+        found = true;
+      // recurse function if next of current node is not null
+      } else if (node.next !== null) {
+        return traverse(node.next);
+      // return false if target value is not found
+      } else {
+        found = false;
+      }
+      return found;
+    };
+    return traverse(start);
   };
-
   return list;
 };
 
