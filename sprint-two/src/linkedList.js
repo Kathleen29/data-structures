80f267c20e497083ff4e01b46acf97797cba7144
Camryn Reid <camrr1991@gmail.com>
2019-10-24 17:20:44 -0700

    Stubbed out very high level approach to methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,21 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // call an instance of Node
+    // somehow update previous node to tell it what next should point to
+    // add new instance of node to list object
   };
 
   list.removeHead = function() {
+    // head gets reassigned to next of current head node
+    // store original node that head points to in variable
+    // return original node variable
   };
 
   list.contains = function(target) {
+    // loop through linked list for target value
+    // return true if target value is found
+    // return false if target value is not found
   };
 
   return list;
